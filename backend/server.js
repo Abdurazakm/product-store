ed6aa8a3caa81efee42b061782116c3c8c3d2f23
abdurazakm <abdulkem5472gmail.com>
2025-08-19 02:40:48 +0300

    to connect fronted api

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = express();
+
+// CORS configuration
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5175", "http://localhost:3000"],
+  credentials: true
+}));
+
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.use("/api/products", productRoutes);
